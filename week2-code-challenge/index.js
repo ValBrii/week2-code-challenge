18f00f9ab75a5c029941ff12079e21adab3d54e8
Valerie <britneyvalerie85@gmail.com>
2024-12-09 15:34:37 +0300

    changes added to index.js

---
+++ week2-code-challenge/index.js
@@ -0,0 +1,10 @@
+// Shopping list array to store items
+let shoppingList = [];
+
+// Select the DOM elements
+const itemInput = document.getElementById('item-input');
+const addButton = document.getElementById('add-btn');
+const clearButton = document.getElementById('clear-btn');
+const itemList = document.getElementById('item-list');
+
+// Function to render the shopping list in the DOM
